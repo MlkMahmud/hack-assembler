b2bf175f3a127c5d32f41392d4823e7679942d73
Malik Mahmud <almalikmahmud@gmail.com>
2025-02-02 12:55:52 +0100

    feat: validate input file arg

--- src/main.c
+++ src/main.c
@@ -1,7 +1,35 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+bool is_asm_file(char *filename);
 
 int main(int argc, char *argv[]) {
-  printf("Watashi wa star!!!");
-  
+  if (argc != 2 || !is_asm_file(argv[1])) {
+    fprintf(
+        stderr,
+        "usage: %s asm_file\n asm_file: an absolute or relative path to a .asm "
+        "file.\n",
+        argv[0]);
+    return 1;
+  }
+
   return 0;
+}
+
+bool is_asm_file(char *filename) {
+  if (!filename) {
+    return false;
+  }
+
+  char extension[] = ".asm";
+  size_t extension_len = strlen((extension));
+  size_t filenmame_len = strlen(filename);
+
+  if (extension_len > filenmame_len) {
+    return false;
+  }
+
+  return strcmp(filename + (filenmame_len - extension_len), extension) == 0;
 }
